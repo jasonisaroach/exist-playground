ada8a96c670ace7ba1745be631f204587af55645
jasonisaroach <jasonisaroach@gmail.com>
2017-06-12 19:55:44 -0400

    Large Amount

--- p5-sandbox/js/sketch-03.js
+++ p5-sandbox/js/sketch-03.js
@@ -40,9 +40,6 @@ function Orb() {
         strokeWeight(1);
         fill(0, 100);
         ellipse(this.x, this.y, this.r, this.r);
-
-        strokeWeight(4);
-        point(this.x, this.y + (this.r / 2) * sin(90));
     }
 }
 
